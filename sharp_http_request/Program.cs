3fb6ecc1726a76aa6591bf7d88e77cb6649eb5ed
athl64 <athl64@bigmir.net>
2014-03-17 23:28:56 +0200

    v2
    almost final, mm... anyway it working )
    
    cmd parameter 'quiet' - close after download complete, like:
    
    'sharp_http_request.exe quiet'

--- sharp_http_request/Program.cs
+++ sharp_http_request/Program.cs
@@ -13,15 +13,47 @@ namespace sharp_http_request
     {
         static void Main(string[] args)
         {
+            string mode = "";
+
+            if (args.Length > 0)
+            {
+                mode = args[0];
+            }
+
+            Console.Write("Wallbase.cc random wallpaper downloader\n\rby Dvixi :)\n\r");
+
             sharp_request req = new sharp_request();
-            string response = req.getR("http://wallbase.cc/random");
-            string pathToFile = "htmlResponse.html";
+            string response = req.getR("http://wallbase.cc/random?section=wallpapers&q=woman&res_opt=eqeq&res=1920x1080&thpp=60");
 
             List<string> lisp = req.parse(response);
-            
-            File.WriteAllText(pathToFile,response);
 
-            Console.ReadKey();
+            foreach (string Line in lisp)
+            {
+                //File.AppendAllText("lines.txt",Line + "\r\n");
+                Console.Write("pull " + Line + " ");
+                if (req.getFile(Line))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("\tOK\n\r");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("\tOOPS...\n\r");
+                    Console.ResetColor();
+                }
+            }
+
+            if (mode == "quiet")
+            {
+                Console.Write("done, closing....");
+            }
+            else
+            {
+                Console.Write("done, press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
 }
--- sharp_http_request/netDown.cs
+++ sharp_http_request/netDown.cs
@@ -34,10 +34,12 @@ namespace sharp_http_request
                 tmpStr = "";
             int start = 0;
             int end = 0;
+            int resInt = 0;
+            bool isNum = false;
             string[] delimiterFrom = { from };
             string[] delimiterTo = { to };
             string[] delimiterM = { middle };
-
+            if (File.Exists("lists.txt")) File.Delete("lists.txt");
             while(xml.IndexOf(from) >= 0 && xml.IndexOf(to) >= 0)
             {
                 start = xml.IndexOf(from);
@@ -45,13 +47,42 @@ namespace sharp_http_request
                 tmpStr = xml.Split(delimiterTo,StringSplitOptions.RemoveEmptyEntries)[0];
                 tmpStr = tmpStr.Split(delimiterFrom, StringSplitOptions.RemoveEmptyEntries)[1];
                 tmpStr = tmpStr.Split(delimiterM, StringSplitOptions.RemoveEmptyEntries)[0];
-                Console.Write("part: " + tmpStr + "\r\n=====================================================\r\n");
-                File.AppendAllText("lines.txt","part: " + tmpStr + "\r\n=====================================================\r\n");
-                result.Add(tmpStr);
+                //Console.Write("part: " + tmpStr + "\r\n=====================================================\r\n");
+                //File.AppendAllText("lines.txt","part: " + tmpStr + "\r\n=====================================================\r\n");
+                isNum = int.TryParse(tmpStr,out resInt);
+                if (isNum)
+                {
+                    result.Add(tmpStr);
+                }
                 xml = xml.Substring(end + 1);
             }
 
             return result;
+        }
+
+        public bool getFile(string URL)
+        {
+            //URL = just number
+            WebClient cl = new WebClient();
+            byte[] tmp;
+
+            if (!System.IO.Directory.Exists("wallbase")) System.IO.Directory.CreateDirectory("wallbase");
+
+            cl.Headers.Add("user-agent", "testing requester");
+
+            try
+            {
+                tmp = cl.DownloadData("http://wallpapers.wallbase.cc/rozne/wallpaper-" + URL + ".jpg");
+                
+                File.WriteAllBytes("wallbase\\wallbase-" + URL + ".jpg", tmp);
+            }
+            catch (WebException e)
+            {
+                return false;
+            }
+            
+
+            return true;
         }
     }
 }
